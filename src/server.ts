7ad3569de7d5e694135a054b3f3bf73c6f963209
Simon Karman <mail@simonkarman.nl>
2022-01-30 14:39:51 +0100

    Added WebSocket.Server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello, World!');
+});
+
+const wss = new WebSocket.Server({ server });
+wss.on('connection', (ws: WebSocket, req) => {
+  console.info('websocket connection: ', req.socket.remoteAddress);
+  ws.on('message', (message: string) => {
+    console.info('websocket received: ', message.toString());
+    ws.send(`Hello, you sent -> ${message}`);
+  });
+  ws.send('Hi there, I am a WebSocket server');
 });
 
 // eslint-disable-next-line no-process-env
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.info(`Example app listening on port ${port}`);
+server.listen(port, () => {
+  console.info(`Listening on port ${port}`);
 });
